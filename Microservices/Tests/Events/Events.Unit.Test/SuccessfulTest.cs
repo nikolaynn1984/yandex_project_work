19030ae57642d4fdfe6f22b19a5829a124ece3d1
Николай Друзин <nikolaynn1984@gmail.com>
2026-09-05 16:57:09 +0300

    Добавление тестов

--- Microservices/Tests/Events/Events.Unit.Test/SuccessfulTest.cs
+++ Microservices/Tests/Events/Events.Unit.Test/SuccessfulTest.cs
@@ -28,6 +28,7 @@ namespace Events.Unit.Test
 
             services.AddScoped<IEventRepository, EventRepository>();
             services.AddScoped<IEventService, EventService>();
+            services.AddScoped<ICacheService, CacheServiceTest>();
 
             this.serviceProvider = services.BuildServiceProvider();
             this.scope = this.serviceProvider.CreateScope();
--- Microservices/Tests/Events/Events.Unit.Test/UnsuccessfulTest.cs
+++ Microservices/Tests/Events/Events.Unit.Test/UnsuccessfulTest.cs
@@ -25,7 +25,7 @@ namespace Events.Unit.Test
 
             services.AddScoped<IEventRepository, EventRepository>();
             services.AddScoped<IEventService, EventService>();
-
+            services.AddScoped<ICacheService, CacheServiceTest>();
 
             this.serviceProvider = services.BuildServiceProvider();
             this.scope = this.serviceProvider.CreateScope();
